75fe66b433a2ea20d02988d19f44376d479dbe68
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-01-05 17:15:34 +0100

    add partial densities member to primitive object

--- src/tests/flow_test_primitive_not_eq.f90
+++ src/tests/flow_test_primitive_not_eq.f90
@@ -4,34 +4,111 @@ program flow_test_primitive_not_eq
 use flow
 
 implicit none
-type(primitive_object) :: primitive1     !< A primitive object.
-type(primitive_object) :: primitive2     !< A primitive object.
-logical                :: test_passed(4) !< List of passed tests.
+type(primitive_object)    :: primitive1           !< A primitive object.
+type(primitive_object)    :: primitive2           !< A primitive object.
+type(field_object_scalar) :: partial_densities(2) !< A scalar field object.
+logical                   :: test_passed(8)       !< List of passed tests.
 
 test_passed = .false.
 
-primitive1%density = 0.125_R_P ; primitive1%velocity = 1._R_P ; primitive1%pressure = 1._R_P
-primitive2%density = 1._R_P    ; primitive2%velocity = 1._R_P ; primitive2%pressure = 1._R_P
+primitive1%density = 0.125_R_P
+primitive1%velocity = 1._R_P
+primitive1%pressure = 1._R_P
+
+primitive2%density = 1._R_P
+primitive2%velocity = 1._R_P
+primitive2%pressure = 1._R_P
 test_passed(1) = (primitive1 /= primitive2).eqv.(.true.)
 print "(A,L1)", 'density  => 0.125 /= 1 = ', test_passed(1)
 
-primitive1%density = 0.125_R_P ; primitive1%velocity = 1._R_P ; primitive1%pressure = 1._R_P
-primitive2%density = 0.125_R_P ; primitive2%velocity = 2._R_P ; primitive2%pressure = 1._R_P
+primitive1%density = 0.125_R_P
+primitive1%velocity = 1._R_P
+primitive1%pressure = 1._R_P
+
+primitive2%density = 0.125_R_P
+primitive2%velocity = 2._R_P
+primitive2%pressure = 1._R_P
 test_passed(2) = (primitive1 /= primitive2).eqv.(.true.)
 print "(A,L1)", 'velocity => 1 /= 2     = ', test_passed(2)
 
-primitive1%density = 0.125_R_P ; primitive1%velocity = 1._R_P ; primitive1%pressure = 1._R_P
-primitive2%density = 0.125_R_P ; primitive2%velocity = 1._R_P ; primitive2%pressure = 2._R_P
+primitive1%density = 0.125_R_P
+primitive1%velocity = 1._R_P
+primitive1%pressure = 1._R_P
+
+primitive2%density = 0.125_R_P
+primitive2%velocity = 1._R_P
+primitive2%pressure = 2._R_P
 test_passed(3) = (primitive1 /= primitive2).eqv.(.true.)
 print "(A,L1)", 'pressure => 1 /= 2     = ', test_passed(3)
 
-primitive1%density = 0.125_R_P ; primitive1%velocity = 1._R_P ; primitive1%pressure = 1._R_P
-primitive2%density = 1._R_P    ; primitive2%velocity = 2._R_P ; primitive2%pressure = 2._R_P
+primitive1%density = 0.125_R_P
+primitive1%velocity = 1._R_P
+primitive1%pressure = 1._R_P
+
+primitive2%density = 1._R_P
+primitive2%velocity = 2._R_P
+primitive2%pressure = 2._R_P
 test_passed(4) = (primitive1 /= primitive2).eqv.(.true.)
 print "(A)", new_line('a')//'all together'
 print "(A,L1)", 'density  => 0.125 /= 1 = ', test_passed(4)
 print "(A,L1)", 'velocity => 1 /= 2     = ', test_passed(4)
 print "(A,L1)", 'pressure => 1 /= 2     = ', test_passed(4)
+
+primitive1%density = 0.125_R_P
+primitive1%velocity = 1._R_P
+primitive1%pressure = 1._R_P
+
+primitive2%density = 0.125_R_P
+primitive2%velocity = 1._R_P
+primitive2%pressure = 1._R_P
+test_passed(5) = (primitive1 /= primitive2).eqv.(.false.)
+print "(A)", new_line('a')//'all together'
+print "(A,L1)", 'density  => 0.125 /= 1 = ', test_passed(5)
+print "(A,L1)", 'velocity => 1 /= 2     = ', test_passed(5)
+print "(A,L1)", 'pressure => 1 /= 2     = ', test_passed(5)
+
+primitive1%density = 1._R_P
+primitive1%velocity = 1._R_P
+primitive1%pressure = 1._R_P
+partial_densities(1) = 0.5_R_P
+partial_densities(2) = 0.5_R_P
+primitive1%partial_densities = partial_densities
+
+primitive2%density = 1._R_P
+primitive2%velocity = 1._R_P
+primitive2%pressure = 1._R_P
+test_passed(6) = (primitive1 /= primitive2).eqv.(.true.)
+print "(A)", new_line('a')//'all together true with densities'
+print "(A,L1)", 'density   => 1   == 1    = ', test_passed(6)
+print "(A,L1)", 'velocity  => 1   == 1    = ', test_passed(6)
+print "(A,L1)", 'pressure  => 1   == 1    = ', test_passed(6)
+print "(A,L1)", 'densities => 0.5 /= null = ', test_passed(6)
+
+primitive2%density = 1._R_P
+primitive2%velocity = 1._R_P
+primitive2%pressure = 1._R_P
+partial_densities(1) = 0.1_R_P
+partial_densities(2) = 0.5_R_P
+primitive2%partial_densities = partial_densities
+test_passed(7) = (primitive1 /= primitive2).eqv.(.true.)
+print "(A)", new_line('a')//'all together true with densities'
+print "(A,L1)", 'density   => 1   == 1   = ', test_passed(7)
+print "(A,L1)", 'velocity  => 1   == 1   = ', test_passed(7)
+print "(A,L1)", 'pressure  => 1   == 1   = ', test_passed(7)
+print "(A,L1)", 'densities => 0.5 /= 0.1 = ', test_passed(7)
+
+primitive2%density = 1._R_P
+primitive2%velocity = 1._R_P
+primitive2%pressure = 1._R_P
+partial_densities(1) = 0.5_R_P
+partial_densities(2) = 0.5_R_P
+primitive2%partial_densities = partial_densities
+test_passed(8) = (primitive1 /= primitive2).eqv.(.false.)
+print "(A)", new_line('a')//'all together true with densities'
+print "(A,L1)", 'density   => 1   == 1   = ', test_passed(8)
+print "(A,L1)", 'velocity  => 1   == 1   = ', test_passed(8)
+print "(A,L1)", 'pressure  => 1   == 1   = ', test_passed(8)
+print "(A,L1)", 'densities => 0.5 == 0.5 = ', test_passed(8)
 
 print "(A,L1)", new_line('a')//'Are all tests passed? ', all(test_passed)
 endprogram flow_test_primitive_not_eq
