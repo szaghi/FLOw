85486f3987b6c4858aa37e2abc70a555b6c7d66a
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-03-23 06:51:10 +0100

    minor changes API

--- src/lib/flow.f90
+++ src/lib/flow.f90
@@ -1,16 +1,20 @@
 !< FLOw, Fortran fLuid Object
+
 module flow
-use flow_field_object
-use flow_field_objects
-use flow_primitive_object
-use penf
+!< FLOw, Fortran fLuid Object
+
+use flow_field_object, only : field_object
+use flow_field_scalar_vectorial, only : field_scalar, field_vectorial
+use flow_primitive_object, only : primitive_object
+use penf, only : I_P, I1P, I2P, I4P, I8P, R_P, R4P, R8P, R16P
+use vecfor, only : vector
 
 implicit none
 private
 public :: field_object
-public :: field_object_scalar
-public :: field_object_vectorial
+public :: field_scalar
+public :: field_vectorial
 public :: primitive_object
-! expose PENF kinds
 public :: I_P, I1P, I2P, I4P, I8P, R_P, R4P, R8P, R16P
+public :: vector
 endmodule flow
