1cdf6ed4058aef2a95e440ea84a30cfac920ee7b
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-07-13 12:13:58 +0200

    update to new PENF API

--- src/tests/flow_test_compressible_transformations.f90
+++ src/tests/flow_test_compressible_transformations.f90
@@ -5,7 +5,7 @@ program flow_test_compressible_transformations
 
 use flow, only : eos_compressible, conservative_compressible, primitive_compressible, &
                  conservative_to_primitive_compressible, primitive_to_conservative_compressible
-use penf, only : R_P, Zero
+use penf, only : R_P, ZeroR_P
 
 implicit none
 type(eos_compressible)          :: eos                 !< An equation of state.
@@ -18,16 +18,16 @@ are_tests_passed = .false.
 eos = eos_compressible(cp=1040.004_R_P, cv=742.86_R_P)
 u = conservative_compressible(density=1._R_P, energy=2.5_R_P)
 p = conservative_to_primitive_compressible(conservative=u, eos=eos)
-are_tests_passed(1) = (p%density  >= 1._R_P - Zero).and.(p%density  <= 1._R_P + Zero).and. &
-                      (p%velocity >= 0._R_P - Zero).and.(p%velocity <= 0._R_P + Zero).and. &
-                      (p%pressure >= 1._R_P - Zero).and.(p%pressure <= 1._R_P + Zero)
+are_tests_passed(1) = (p%density  >= 1._R_P - ZeroR_P).and.(p%density  <= 1._R_P + ZeroR_P).and. &
+                      (p%velocity >= 0._R_P - ZeroR_P).and.(p%velocity <= 0._R_P + ZeroR_P).and. &
+                      (p%pressure >= 1._R_P - ZeroR_P).and.(p%pressure <= 1._R_P + ZeroR_P)
 print "(A,L1)", 'u to p, is done right? ', are_tests_passed(1)
 
 p = primitive_compressible(density=1._R_P, pressure=1._R_P)
 u = primitive_to_conservative_compressible(primitive=p, eos=eos)
-are_tests_passed(2) = (u%density  >= 1._R_P  - Zero).and.(u%density  <= 1._R_P  + Zero).and. &
-                      (u%momentum >= 0._R_P  - Zero).and.(u%momentum <= 0._R_P  + Zero).and. &
-                      (u%energy   >= 2.5_R_P - Zero).and.(u%energy   <= 2.5_R_P + Zero)
+are_tests_passed(2) = (u%density  >= 1._R_P  - ZeroR_P).and.(u%density  <= 1._R_P  + ZeroR_P).and. &
+                      (u%momentum >= 0._R_P  - ZeroR_P).and.(u%momentum <= 0._R_P  + ZeroR_P).and. &
+                      (u%energy   >= 2.5_R_P - ZeroR_P).and.(u%energy   <= 2.5_R_P + ZeroR_P)
 print "(A,L1)", 'p to u, is done right? ', are_tests_passed(2)
 
 print "(A,L1)", new_line('a')//'Are all tests passed? ', all(are_tests_passed)
